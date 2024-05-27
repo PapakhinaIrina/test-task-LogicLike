5a71914d8af4bddc36ba0dd7f734c32a125f129d
Irina Papakhina <papahinairina@gmail.com>
2024-05-27 12:32:40 +0300

    feat:added menuList

--- logic-like/src/app/App.tsx
+++ logic-like/src/app/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
 import "./App.css";
+import { Courses } from "../pages/Courses";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Courses />
+    </div>
+  );
 }
 
 export default App;
---
+++ logic-like/src/shared/api/models/Cards.ts
@@ -0,0 +1,8 @@
+export type Course = {
+  bgColor: string;
+  id: string;
+  image: string;
+  name: string;
+  tags: string[];
+  length: number;
+};
